9acfb703bd62c2bc0ffb9a5028cf172ae16a0d04
french49driver <french.driver49@gmail.com>
2022-04-12 19:00:54 +0200

    Ajout du serveur Shoutcast

---
+++ config/config.js
@@ -0,0 +1,5 @@
+var g = '80.84.57.76'; // URL SANS HTTP:// - IP WITHOUT HTTP:// - Radionomy : streaming.radionomy.com
+var h = '6183'; // PORT - Radionomy : 80
+var z = ''; //Shoutcast='/;stream.mp3'
+var i = ''; // Icecast='/mount' - Shoutcast='' - Radionomy : /radioname
+var l = ''; // Radionomy GUID (For Current Listen)
